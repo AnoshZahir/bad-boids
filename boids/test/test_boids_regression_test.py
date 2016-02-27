4213736bbdb27aa7c62b03456ae30e51464c013a
AnoshZahir <anoshzahir@hotmail.com>
2016-02-27 17:26:13 +0000

    updated test to reflect change to the script where class Boid was added.

--- boids/test/test_boids_regression_test.py
+++ boids/test/test_boids_regression_test.py
@@ -1,4 +1,4 @@
-from boids import update_boids
+from ..boids import Boids
 from nose.tools import assert_almost_equal
 import os
 import yaml
@@ -6,7 +6,8 @@ import yaml
 def test_bad_boids_regression():
     regression_data=yaml.load(open(os.path.join(os.path.dirname(__file__),'fixture.yml')))
     boid_data=regression_data["before"]
-    update_boids(boid_data)
+    boids = Boids()
+    boids.update_boids(boid_data)
     for after,before in zip(regression_data["after"],boid_data):
         for after_value,before_value in zip(after,before): 
             assert_almost_equal(after_value,before_value,delta=0.01)

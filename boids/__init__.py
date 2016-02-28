c0c06d076cfe7c3a0db0e66d6890f14831fc45df
AnoshZahir <anoshzahir@hotmail.com>
2016-02-28 21:35:41 +0000

    added command line arguments and config file loading from CMI.

--- boids/__init__.py
+++ boids/__init__.py
@@ -1,1 +1,37 @@
-#creating folder structure - to be updated
+from boids import Boids
+from argparse import ArgumentParser
+from ConfigParser import ConfigParser
+import json
+
+def main():
+    
+    parser = ArgumentParser(description = "A simulation of a flock of birds.")
+
+    parser.add_argument('--config', '-c', default = 'config.cfg', help = 'select config file')
+    arguments = parser.parse_args()
+
+    config = ConfigParser.RawConfigParser()
+    with open(arguments.config) as config_file:
+        config.read(config_file)
+        no_of_boids = config.getint('boids_initiate', 'no_of_boids')
+        position_limits =  json.loads(config.get('boids_initiate', 'position_limits'))
+        velocity_limits =  json.loads(config.get('boids_initiate', 'velocity_limits'))
+        move_to_middle_strength = config.getfloat('boids_initiate', 'move_to_middle_strength')
+        alert_distance = config.getint('boids_initiate', 'alert_distance')
+        formation_flying_distance = config.getfloat('boids_initiate', 'formation_flying_distance')
+        formation_flying_strength = config.getfloat('boids_initiate', 'formation_flying_strength')
+
+        xlim = json.loads(config.get('run_simulation','xlim'))
+        ylim = json.loads(config.get('run_simulation', 'ylim'))
+        frames = config.getint('run_simulation', 'frames')
+        interval = config.getint('run_simulation', 'interval')
+            
+        boids = Boids(no_of_boids = no_of_boids, position_limits = position_limits,
+            velocity_limits = velocity_limits,
+            move_to_middle_strength = move_to_middle_strength,
+            alert_distance = alert_distance,
+            formation_flying_distance = formation_flying_distance,
+            formation_flying_strength = formation_flying_strength)
+    
+        boids.run_simulation(xlim = tuple(xlim), ylim = tuple(ylim), frames = frames,
+                          interval = interval)

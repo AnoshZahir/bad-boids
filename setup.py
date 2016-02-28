75c5f59beffafc3837960a4942eb038050b52556
AnoshZahir <anoshzahir@hotmail.com>
2016-02-28 17:11:30 +0000

    first commit.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "Bad boids",
+    version = "0.1",
+    description = "A simulation of a flock of birds.",
+    url = "https://github.com/AnoshZahir/bad_boids.git",
+    licence = "MIT",
+    author = "Anosh Zahir",
+    author_email = "anosh.zahir15@imperial.ac.uk",
+    packages = find_packages(exclude = ['*test']),
+    scripts = ['scripts/boids'],
+    install_requires = ['argparse', 'matplotlib', 'numpy', 'nose', 'mock' ]
+)
+
+ 

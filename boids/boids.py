f3219d4e8903e66762c0f8a7ffca8a93ccefef2b
AnoshZahir <anoshzahir@hotmail.com>
2016-02-28 17:07:05 +0000

    smell repeated forloops.  vectorised the code inside update_boids.

--- boids/boids.py
+++ boids/boids.py
@@ -8,37 +8,42 @@ class Boids(object):
         self.no_of_boids = no_of_boids
         self.positions = self.new_flock(self.no_of_boids, np.array([-450, 300]), np.array([50,600]))
         self.velocities = self.new_flock(self.no_of_boids, np.array([0, -20]), np.array([10,20]))
-                
+        #self.boids = (self.positions, self.velocities)    
+    
     def new_flock(self, no_of_boids, lower_limits, upper_limits):
         width = upper_limits - lower_limits
         return (lower_limits[:, np.newaxis] + np.random.rand(2,no_of_boids)*width[:, np.newaxis])
+    
         
-    def update_boids(self, positions, velocities):
-	xs,ys = positions
-	xvs,yvs= velocities
+    def update_boids(self, positions, velocities, move_to_middle_strength = 0.01,
+                alert_distance = 100, formation_flying_distance = 10000, 
+                formation_flying_strength = 0.125):
+	
 	# Fly towards the middle
-	for i in range(len(xs)):
-		for j in range(len(xs)):
-			xvs[i]=xvs[i]+(xs[j]-xs[i])*0.01/len(xs)
-	for i in range(len(xs)):
-		for j in range(len(xs)):
-			yvs[i]=yvs[i]+(ys[j]-ys[i])*0.01/len(xs)
+	middle = np.mean(positions, 1)
+        direction_to_middle = positions - middle[:, np.newaxis]
+        velocities -= direction_to_middle * move_to_middle_strength
+	
 	# Fly away from nearby boids
-	for i in range(len(xs)):
-		for j in range(len(xs)):
-			if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 100:
-				xvs[i]=xvs[i]+(xs[i]-xs[j])
-				yvs[i]=yvs[i]+(ys[i]-ys[j])
+	separations = positions[:, np.newaxis, :] - positions[:, :, np.newaxis]
+        squared_displacements = separations * separations
+        square_distances = np.sum(squared_displacements, 0)
+        far_away = square_distances > alert_distance
+        separations_if_close = np.copy(separations)
+        separations_if_close[0,:,:][far_away] = 0
+        separations_if_close[1,:,:][far_away] = 0
+        velocities += np.sum(separations_if_close,1)
+        
 	# Try to match speed with nearby boids
-	for i in range(len(xs)):
-		for j in range(len(xs)):
-			if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 10000:
-				xvs[i]=xvs[i]+(xvs[j]-xvs[i])*0.125/len(xs)
-				yvs[i]=yvs[i]+(yvs[j]-yvs[i])*0.125/len(xs)
+        velocity_differences = velocities[:,np.newaxis,:] - velocities[:,:,np.newaxis]
+        very_far=square_distances > formation_flying_distance
+        velocity_differences_if_close = np.copy(velocity_differences)
+        velocity_differences_if_close[0,:,:][very_far] = 0
+        velocity_differences_if_close[1,:,:][very_far] = 0
+        velocities -= np.mean(velocity_differences_if_close, 1) * formation_flying_strength
+	
 	# Move according to velocities
-	for i in range(len(xs)):
-		xs[i]=xs[i]+xvs[i]
-		ys[i]=ys[i]+yvs[i]
+        positions += velocities
 
     def run_simulation(self):
          figure = plt.figure()
@@ -54,3 +59,4 @@ class Boids(object):
 if __name__ == "__main__":
     boids = Boids()
     boids.run_simulation()
+   

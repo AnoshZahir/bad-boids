5df8e051c31ad5296f5107af66116cd84dfe5fce
AnoshZahir <anoshzahir@hotmail.com>
2016-02-29 16:12:54 +0000

    added entry points.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author_email = "anosh.zahir15@imperial.ac.uk",
     packages = find_packages(exclude = ['*test']),
     install_requires = ['argparse', 'matplotlib', 'numpy', 'nose', 'mock'],
-    entry_points={'console_scripts': ['bad_boids=boids:main']},
+    entry_points={'console_scripts': ['bad_boids=boids:main']}
 )
 
  

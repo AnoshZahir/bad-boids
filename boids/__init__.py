0966b1e5852f377995836f89a863bd08d4402752
AnoshZahir <anoshzahir@hotmail.com>
2016-02-29 16:11:42 +0000

    RawConfigParser changed to ConfigParser.

--- boids/__init__.py
+++ boids/__init__.py
@@ -1,23 +1,22 @@
 from boids import Boids
 from argparse import ArgumentParser
-from ConfigParser import ConfigParser
+import ConfigParser
 import json
 
 def main():
     
     parser = ArgumentParser(description = "A simulation of a flock of birds.")
-
     parser.add_argument('--config', '-c', default = 'config.cfg', help = 'select config file')
     arguments = parser.parse_args()
 
-    config = ConfigParser.RawConfigParser()
+    config = ConfigParser.ConfigParser()
     with open(arguments.config) as config_file:
-        config.read(config_file)
+        config.readfp(config_file)
         no_of_boids = config.getint('boids_initiate', 'no_of_boids')
         position_limits =  json.loads(config.get('boids_initiate', 'position_limits'))
         velocity_limits =  json.loads(config.get('boids_initiate', 'velocity_limits'))
         move_to_middle_strength = config.getfloat('boids_initiate', 'move_to_middle_strength')
-        alert_distance = config.getint('boids_initiate', 'alert_distance')
+        alert_distance = config.getfloat('boids_initiate', 'alert_distance')
         formation_flying_distance = config.getfloat('boids_initiate', 'formation_flying_distance')
         formation_flying_strength = config.getfloat('boids_initiate', 'formation_flying_strength')
 
